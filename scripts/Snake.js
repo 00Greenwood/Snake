a527a8aa5cd3bb7a377f7a2ecee0d0c6d0265926
00Greenwood <marcus_greenwood@hotmail.co.uk>
2020-06-21 13:16:19 +0100

    Add snake class

---
+++ scripts/Snake.js
@@ -0,0 +1,6 @@
+// class representation of the Snake, contains the head node of a linked list.
+class Snake {
+  constructor(position) {
+    this.head = new SnakeNode();
+  }
+}
---
+++ scripts/SnakeNode.js
@@ -0,0 +1,6 @@
+// class representation of a Snake Node, acts as element in a link list. 
+class SnakeNode {
+  constructor(parent) {
+      this.parent = parent;
+  }
+}
